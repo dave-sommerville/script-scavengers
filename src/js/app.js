28bfe70357fe29e207ca886f4b1c2335925eb92d
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-11-25 19:50:52 -0600

    modules introduced

--- src/js/app.js
+++ src/js/app.js
@@ -10,39 +10,10 @@
 	
 */
 /*-------------------------------------------------------------------------->
-		Word bank
-<--------------------------------------------------------------------------*/
-const wordBank = [
-  'wrench', 'oil', 'brake', 'rust', 'tire', 'engine', 'gear', 'metal', 'scrap', 
-  'bolt', 'muffler', 'wire', 'spanner', 'junk', 'clutch', 'fuel', 'gasket', 
-  'radiator', 'plumber', 'hose', 'piston', 'axle', 'carburetor', 'panel', 
-  'nut', 'exhaust', 'frame', 'battery', 'spark', 'scrapheap', 'tread', 
-  'workshop', 'grime', 'grinder', 'engineer', 'toolbox', 'grease', 'shock', 
-  'pump', 'steering', 'mechanic', 'fender', 'clamp', 'drill', 'chassis', 
-  'gravel', 'cog', 'track', 'bolt', 'bushing', 'washer', 'clamp', 'valve', 
-  'accelerator', 'discard', 'junkyard', 'tractor', 'spare', 'trailer', 
-  'rivet', 'plunger', 'scraper', 'welding', 'chain', 'ratchet', 'compressor', 
-	'bucket', 'leak', 'forklift', 'patch', 'screws', 'wheel', 'spike', 'cylinder', 
-	'cable', 'pneumatic', 'bumper', 'shredder', 'frame', 'hydraulic', 'drain', 
-	'exhaust', 'dynamo', 'grinder', 'axle', 'hydraulic', 'propeller', 'bench', 
-  'alternator', 'flywheel', 'carb', 'fuse', 'dismantle', 'magnet', 'torch', 
-	'plug', 'filter', 'dismantler', 'scrapper', 'leaks', 'fuel', 'cell',
-	'transmission', 'coil', 'valve', 'hubcap', 'scrapheap', 'junkpile', 
-	'discarded', 'rusty', 'tools', 'gearbox', 'piston', 'detox', 'seatbelt', 
-	'jacket', 'fluid', 'filter', 'radiator', 'starter', 'tire', 'battery', 
-	'terminal', 'ratchet', 'socket', 'pipe', 'harness', 'headlight', 'grinder', 
-	'clutch','scrap', 'bracket', 'spanner', 'paneling', 'compressor', 
-	'rebar','fender', 'panel', 'gasket', 'oilcan', 'fitting', 'toolbox', 'torch', 
-	'hopper', 'coil', 'junk', 'debris', 'rebar', 'chain', 'steel', 'bolt', 
-	'cutters', 'wheel', 'ratchets', 'mask', 'scrapwire', 'hammer', 'handle', 
-	'lead', 'manifold', 'wrench', 'handtool', 'welder', 'door', 'grindstone', 
-	'shocks', 'absorber', 'strut', 'hydraulic', 'flywheel', 'exhaust', 'cargo', 
-	'coolant', 'brakes'
-];
-
-/*-------------------------------------------------------------------------->
 		Class Declartation
 <--------------------------------------------------------------------------*/
+import { wordBank } from "./word-bank.js";
+
 class Score {
 	#date;
 	#hits;
